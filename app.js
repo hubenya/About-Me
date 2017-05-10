7b760c1a4c2127ce6c98357780ace8e7a6af2e19
Slavik <hubenya@hotmail.com>
2017-05-09 17:04:25 -0700

    CSS update

--- app.js
+++ app.js
@@ -4,38 +4,38 @@ var user = prompt('What is your name?');
   console.log('user:' + user);
   alert('Hi there, ' + user + ', I\'m going to ask you few question about me.');
 
-var birthplace = prompt(user + ', this is a yes/no question.  Please answer with Yes or No. Was I born in US?');toUpperCase();
+var birthplace = prompt(user + ', this is a yes/no question.  Please answer with Yes or No. Was, I born in US?').toUpperCase();
   console.log('user birthplace:' + birthplace);
-  if (birthplace === 'Yes') {
-  alert('Nice try, but I was born in Ukraine.');
-} else if (birthplace === 'No') {
-  alert('Greate Job!!! You are correct, I was born outside of US.');
-  } else {
-  alert(user + ' It appears that you did not answer the question.');
-  }
+    if (birthplace === 'YES') {
+    alert('Nice try, but I was born in Ukraine.');
+    } else if (birthplace === 'NO') {
+    alert('Greate Job!!! You are correct, I was born outside of US.');
+    } else {
+    alert(user + ' It appears that you did not answer the question.');
+    }
 
-  var movedToUs = prompt('This is a yes/no question just like previous. Take your best guess, do you think I moved to US before my 10th birthday?'); toUpperCase();
-    console.log('user movedToUs:' + movedToUs);
-    if (movedToUs === 'Yes') {
+var movedToUs = prompt('This is a yes/no question just like previous. Take your best guess, do you think I moved to US before my 10th birthday?').toUpperCase();
+  console.log('user movedToUs:' + movedToUs);
+    if (movedToUs === 'YES') {
     alert('Nice try, I moved to US in 2003.');
-  } else if (movedToUs === 'No') {
+    } else if (movedToUs === 'NO') {
     alert('Good Job!!! You guessed right.');
     } else {
     alert(user + ' It appears that you did not answer the question.');
     }
 
-  var highSchool = prompt('What year do you think I graduated from high school?  I will give you few hints 2001, 2007, 2015');
-    console.log('user highSchool:' + highSchool);
+var highSchool = prompt('What year do you think I graduated from high school?  I will give you few hints 2001, 2007, 2015');
+  console.log('user highSchool:' + highSchool);
     if (highSchool === '2007') {
     alert('Nice job. How did you know? Where you in my class?');
-  } else if (highSchool === '2001' || '2015') {
+    } else if (highSchool === '2001' || '2015') {
     alert('Nice try.');
     } else {
     alert(user + ' It appears that you did not answer the question.');
     }
 
-  var kids = prompt('This is a yes/no question. Do you think I have kids?'); toLowerCase();
-    console.log('user kids:' + kids);
+var kids = prompt('This is a yes/no question. Do you think I have kids?').toLowerCase();
+  console.log('user kids:' + kids);
     if (kids === 'yes') {
     alert('You are correct, I have one baby girl.');
     } else if (kids === 'no') {
@@ -44,8 +44,8 @@ var birthplace = prompt(user + ', this is a yes/no question.  Please answer with
     alert(user + ' It appears that you did not answer the question.');
     }
 
-  var funFact = prompt('Do you think I like fishing or snowboarding?');
-    console.log('user funFact:' + funFact);
+var funFact = prompt('Do you think I like fishing or snowboarding?').toLowerCase();
+  console.log('user funFact:' + funFact);
     if (funFact === 'fishing' || 'snowboarding') {
     alert('Nice try but I don\'t do either of them.');
     } else {
