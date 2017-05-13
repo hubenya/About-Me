f24f73cbca7fd4311178746ca35442c787c9e1af
Slavik <hubenya@hotmail.com>
2017-05-13 13:59:08 -0700

    Fixed my Y-N user inputs and added for loop and while loop

--- app.js
+++ app.js
@@ -57,3 +57,50 @@ if (funFact === 'fishing' && 'snowboarding') {
 } else {
   alert(user + ' It appears that you did not answer the question.');
 }
+
+var myFavNumber = 7;
+
+for (var i=0; i < 4; i++){
+  var answer = parseInt(prompt('Do you know my favorite numner between 0 & 10?'));
+  console.log('number:' + answer);
+  if (answer < myFavNumber) {
+    alert('You are a little too low with your answer :\(');
+  } else if (answer > myFavNumber) {
+    alert('Your answer is a little too hight :\(');
+//this logic is not working
+  } else if (answer === NaN && answer === null) {
+    alert('Please enter a number please');
+  } else {
+    alert('You got it right my number is 7.');
+    break;
+  }
+}
+
+//here is where i added while loop this is a success
+
+var carName = ['toyota', 'dodge', 'acura'];
+var userAnswer = 0;
+var counter = 1;
+
+while(userAnswer < 6){
+  var answer1 = prompt('What car brans do you think I own?').toLowerCase();
+  for (var j=0; i < carName.length; i++) {
+    console.log(carName);
+    if (carName[j] === answer1) {
+      alert('Good guess, I do own ' + answer1 + '. It appears that you have tried ' + counter + '.');
+      userAnswer = 6;
+      break;
+    }
+  }
+  userAnswer++;
+  if (userAnswer < 6){
+    alert('You were so close but try again');
+    counter++;
+  //after few hours i got this to work but not fully. this was part of our requirement
+  } else if (userAnswer > 6 || answer1 !== carName[j]) {
+    alert('It appears that you have tried over ' + counter + ' times and there are no more tries. Sorry!!!');
+    break;
+  }
+  console.log(answer1);
+  console.log(userAnswer);
+}
